7267bdf9787a97baab1f9bf4304d0c2d204f29ce
LilTzar <eeoerkeedu@gmail.com>
2022-07-25 11:56:40 -0600

    solved character count

--- 01-character-count/Unsolved/character-count.js
+++ 01-character-count/Unsolved/character-count.js
@@ -1,3 +1,17 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+const characterCount = function (str) {
+	const brokenStr = str.split("");
+	console.log(brokenStr);
+	const letters = {};
+	brokenStr.forEach((char) => {
+		if (!letters[char]) {
+			letters[char] = 1;
+		} else {
+			letters[char] = letters[char] + 1;
+			console.log("found");
+		}
+	});
+	console.log(letters);
+	return letters;
+};
